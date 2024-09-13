e880b52c84bc252a451c131bff7fa41a8aab21be
choiminkyung <choialsrud2002@gmail.com>
2024-09-13 15:06:29 +0900

    Initial Commit

---
+++ TimetravelDiary/TimetravelDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimetravelDiaryApp.swift
+//  TimetravelDiary
+//
+//  Created by 최민경 on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TimetravelDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
